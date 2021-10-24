635fd9a5e48183a2d7133bc7a82153b97900d934
Kevin Klein 1 <KevinKlein1@kevins-mbp-3.localdomain>
2021-10-24 20:24:21 +0200

    cases for compound and not compound diagnosises are formulated

--- disease_detection.py
+++ disease_detection.py
@@ -10,24 +10,33 @@ disease_dict = {"Arterienriss": "I77.2",
 
 
 def matching_codes(string):
-    string_list = string.split()
-    if 'des' in string_list:
-        if string_list[2][-2:] == 'es':
-            string_list[2] = string_list[2][:-2]
-        elif string_list[2][-1:] == 's':
-            string_list[2] = string_list[2][:-1]
-
-    string_list.pop(1)
-    assert len(string_list) == 2
 
     var = "not found"
 
-    for word in disease_dict.keys():
-        if string_list[0].lower() in word.lower() and string_list[1].lower() in word.lower():
-            var = disease_dict[word]
+    string_list = string.split()
+
+    if len(string_list) == 3:  # Fall des split up words
+        if 'des' in string_list:
+            if string_list[2][-2:] == 'es':  # 2. Fall kann zweifach auftreten mit es oder s als Endungszusatz
+                string_list[2] = string_list[2][:-2] # Hier nehmen wir die ursprüngliche Form des Nominativs an, Zungengrundes => Zungengrund
+            elif string_list[2][-1:] == 's':
+                string_list[2] = string_list[2][:-1]
+
+        string_list.pop(1)
+        assert len(string_list) == 2
+
+        for word in disease_dict.keys():
+            if string_list[0].lower() in word.lower() and string_list[1].lower() in word.lower(): # Suchen der beiden Wörter des nicht zusammengesetzen Wortes im compound word
+                var = disease_dict[word]
+
+    elif len(string_list) == 1: # Fall zusammengesetztes Nomen
+
+        for word in disease_dict.keys():
+            if string_list[0].lower() == word.lower():
+                var = disease_dict[word]
+
 
     return var
-
 
 
 
